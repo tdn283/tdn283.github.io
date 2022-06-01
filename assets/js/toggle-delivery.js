b90fbf497c46ef3cb6e16bdc3b38c59d2ec45dea
tdn283 <nhatflame@gmail.com>
2022-06-01 17:22:34 +0700

    add new files

---
+++ assets/js/toggle-delivery.js
@@ -0,0 +1,4 @@
+function onClickDrop() {
+    document.getElementById("myDropdown").classList.toggle("product__delivery-clickShow");
+    document.getElementsById("hideBorder").classList.toggle("toggle-off-border-bottom");
+}
